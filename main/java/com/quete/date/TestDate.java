f4ebefa8c6b45e254a56e43db56d65d1e71395cc
Hilary <a.hilary@wanadoo.fr>
2019-07-23 16:01:22 +0200

    quete ok

--- main/java/com/quete/date/TestDate.java
+++ main/java/com/quete/date/TestDate.java
@@ -1,3 +1,5 @@
+package com.quete.date;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -6,7 +8,7 @@ import java.util.Scanner;
 
 class TestDate {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Please fill a day (1-31) :");
@@ -18,6 +20,15 @@ class TestDate {
         sc.close();
 
         // TODO : your code after this line
-        
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        Calendar c = new GregorianCalendar(year,month-1,day);
+        Date d = c.getTime();
+
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+
+        System.out.println("The date is : " + sdf.format(d));
+        System.out.println("The day of week is : " +dayOfWeek);
+
+
     }
 }
